13809f92e70d1f8f85c0fe31eb71517d330b0d26
porscheitng04 <66070227@kmitl.ac.th>
2026-02-10 15:45:00 +0700

    cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Ittiphat"
+last_name = "Ngowkeeree"
+
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "Ittiphat"
+last_name = "Ngowkeeree"
+
+whole_name = last_name + " " + first_name
+
+print(whole_name)
