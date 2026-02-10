4aea595cb647c0515423aaeeb40e0d9deafd81fa
porscheitng04 <66070227@kmitl.ac.th>
2026-02-10 15:46:30 +0700

    cell03

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+if n > 25:
+    print("Error")
+else:
+    while n <= 25:
+        print("Inside the loop, my variable is", n)
+        n += 1
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+while True:
+    msg = input("What you gotta say?: ")
+    if msg == "STOP":
+        break
+    print("I got that! Anything else?")
+    
